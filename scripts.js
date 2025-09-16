49e2e519e2f04e02d86b06dcc51c24baaa60c08b
Luke <uk090961@student.uni-kassel.de>
2025-09-16 18:31:29 +0200

    refactor: move style and scripts to seperate files

---
+++ scripts.js
@@ -0,0 +1,8 @@
+const colors = ['#093145', '#107896', '#829356', '#BCA136', '#C2571A', '#9A2617']
+
+function randomColor() {
+    const i = Math.floor(Math.random() * colors.length)
+    document.body.style.backgroundColor = colors[i]
+}
+
+document.addEventListener("DOMContentLoaded", randomColor);
